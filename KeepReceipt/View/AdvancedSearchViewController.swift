8b915258009d33634cfde0893d03ed008dc40714
Frank Jia <jiafrank98@gmail.com>
2019-01-25 08:00:53 -0800

    laid out storyboard for search

---
+++ KeepReceipt/View/AdvancedSearchViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AdvancedSearchViewController.swift
+//  KeepReceipt
+//
+//  Created by Frank Jia on 2019-01-25.
+//  Copyright © 2019 Frank Jia. All rights reserved.
+//
+
+import UIKit
+
+class AdvancedSearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
