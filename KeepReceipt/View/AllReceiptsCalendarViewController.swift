878bca9c0cc886d73c6613d012bb7b490fd13d38
Frank Jia <jiafrank98@gmail.com>
2019-01-27 20:16:46 -0800

    started implemntation, but can't figure out how to only get month view

---
+++ KeepReceipt/View/AllReceiptsCalendarViewController.swift
@@ -0,0 +1,77 @@
+//
+//  AllReceiptsCalendarViewController.swift
+//  KeepReceipt
+//
+//  Created by Frank Jia on 2019-01-27.
+//  Copyright © 2019 Frank Jia. All rights reserved.
+//
+
+import UIKit
+import FSCalendar
+
+class AllReceiptsCalendarViewController: UIViewController, FSCalendarDataSource, FSCalendarDelegate, UIGestureRecognizerDelegate  {
+
+    @IBOutlet weak var calendar: FSCalendar!
+    @IBOutlet weak var calendarHeightConstraint: NSLayoutConstraint!
+    
+    fileprivate lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy/MM/dd"
+        return formatter
+    }()
+    fileprivate lazy var scopeGesture: UIPanGestureRecognizer = {
+        [unowned self] in
+        let panGesture = UIPanGestureRecognizer(target: self.calendar, action: #selector(self.calendar.handleScopeGesture(_:)))
+        panGesture.delegate = self
+        panGesture.minimumNumberOfTouches = 1
+        panGesture.maximumNumberOfTouches = 2
+        return panGesture
+        }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if UIDevice.current.model.hasPrefix("iPad") {
+            self.calendarHeightConstraint.constant = 400
+        }
+        
+        self.calendar.select(Date())
+        self.view.addGestureRecognizer(self.scopeGesture)
+        self.calendar.scope = .month
+        
+        
+    }
+    
+    // MARK:- UIGestureRecognizerDelegate
+    
+    func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        let velocity = self.scopeGesture.velocity(in: self.view)
+        switch self.calendar.scope {
+        case .month:
+            return velocity.y < 0
+        case .week:
+            return velocity.y > 0
+        }
+        
+        return true
+    }
+    
+    func calendar(_ calendar: FSCalendar, boundingRectWillChange bounds: CGRect, animated: Bool) {
+        self.calendarHeightConstraint.constant = bounds.height
+        self.view.layoutIfNeeded()
+    }
+    
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        print("did select date \(self.dateFormatter.string(from: date))")
+        let selectedDates = calendar.selectedDates.map({self.dateFormatter.string(from: $0)})
+        print("selected dates is \(selectedDates)")
+        if monthPosition == .next || monthPosition == .previous {
+            calendar.setCurrentPage(date, animated: true)
+        }
+    }
+    
+    func calendarCurrentPageDidChange(_ calendar: FSCalendar) {
+        print("\(self.dateFormatter.string(from: calendar.currentPage))")
+    }
+
+}
